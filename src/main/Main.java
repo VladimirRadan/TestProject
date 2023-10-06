f884518cf8b883152e41d4c53aff77db891e7cdf
Vladimir Radan <vladimir.radan@1nce.com>
2023-10-06 18:54:07 +0200

    Resolved conflicts

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,8 @@ public class Main {
             System.out.println(s);
         }
 
+        System.out.println("Ispis new branch - old code!");
+
         System.out.println("Ispis sa branch-a add-new-tests");
 
     }
