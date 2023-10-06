78f0637f751a60106f0405f8c58e373d5153a0e0
Vladimir Radan <vladimir.radan@1nce.com>
2023-10-06 18:18:41 +0200

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] mojNiz = {"Pera", "Zika", "Laza"};
+        mojNiz[0] = "Marko";
+        mojNiz[1] = "John";
+        mojNiz[2] = "Djordje";
+
+        for (String s:mojNiz) {//has next
+            System.out.println(s);
+        }
+
+    }
+
+}
